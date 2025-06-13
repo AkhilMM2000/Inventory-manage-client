3cfb21be0efe19de3f02fce82d007a30dfd8e951
Akhil <manojakhil36@gmail.com>
2025-06-13 17:53:27 +0530

    chore: initial Vite + React + TypeScript + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+
+  return (
+   <>
+   <div>
+
+
+   </div>
+   <h1 className='text-5xl bg-amber-100 place-items-end-safe'>hello world</h1>
+   </>
+      
+  )
+}
+
+export default App
