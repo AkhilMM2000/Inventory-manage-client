41df3f5fb5e8afe5bb69ec91771dd764991f776f
Akhil <manojakhil36@gmail.com>
2026-08-14 12:58:38 +0530

    chore: verify client build setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'react-hot-toast'
 
+console.log('Client app started')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
